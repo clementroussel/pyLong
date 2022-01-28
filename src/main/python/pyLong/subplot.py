5bb9c2a8e048e7176239c309a725925ed2500e50
Clément Roussel <clement.roussel@onf.fr>
2022-01-28 15:31:19 +0100

    Create subplot.py

---
+++ src/main/python/pyLong/subplot.py
@@ -0,0 +1,18 @@
+class Subplot:
+    def __init__(self):
+        self.identifiant = ""
+
+        self.subdivisions = 1
+
+        self.yAxisProperties = {'min': -100.,
+                                'max': 100.,
+                                'label': '',
+                                'intervals': 5,
+                                'label color': 'Black',
+                                'value color': 'Black',
+                                'lower shift': 0,
+                                'upper shift': 0}
+        
+        self.legend = {'active': True,
+                       'position': 'upper left',
+                       'columns': 6}
