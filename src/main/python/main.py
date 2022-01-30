d77dd999d14b7158d65a652143d12ff287a93c02
Clément Roussel <clement.roussel@onf.fr>
2022-01-30 20:30:12 +0100

    Update main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,29 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+
+from mainWindow import MainWindow
 
 import sys
+import time
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
+    appctxt.app.setStyle("Fusion")
+
+    # Create and display the splash screen
+    splashImage = QPixmap(appctxt.get_resource('images/splashImage_600px.png'))
+    splashScreen = QSplashScreen(splashImage)
+    splashScreen.show()
+
+    appctxt.app.processEvents()
+
+    time.sleep(3)
+
+    # Create and display the main window
+    mainWindow = MainWindow(appctxt)
+    mainWindow.show()
+
+    # Remove the splash screen
+    splashScreen.finish(mainWindow)
+
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
