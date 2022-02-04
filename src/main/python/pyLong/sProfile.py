92d187b7a62230a00eab0c44229e955eab63b3e4
Clément Roussel <clement.roussel@onf.fr>
2022-02-04 15:04:25 +0100

    Update sProfile.py

--- src/main/python/pyLong/sProfile.py
+++ src/main/python/pyLong/sProfile.py
@@ -54,6 +54,8 @@ class sProfile:
         self.linePercents = Line2D([], [])
 
         self.lineDegrees = Line2D([], [])
+
+        self.trickLine = Line2D([], [])
             
     def updateData(self, x, z):
         if z[0] > z[-1]:
@@ -71,47 +73,60 @@ class sProfile:
         self.linePercents.set_data(self.x, self.slopesPercents)
         self.lineDegrees.set_data(self.x, self.slopesDegrees)
 
-        if not self.active or not self.markersVisible:
-            self.line.set_label("")
-            self.linePercents.set_label("")
-            self.lineDegrees.set_label("")
+        # if not self.active or not self.markersVisible:
+        #     self.line.set_label("")
+        #     self.linePercents.set_label("")
+        #     self.lineDegrees.set_label("")
             
-        else:
-            self.line.set_label(self.label)
-            self.linePercents.set_label(self.label)
-            self.lineDegrees.set_label(self.label)
+        # else:
+        #     self.line.set_label(self.label)
+        #     self.linePercents.set_label(self.label)
+        #     self.lineDegrees.set_label(self.label)
+
+        self.line.set_label("")
+        self.linePercents.set_label("")
+        self.lineDegrres.set_label("")
+        self.trickLine.set_label(self.label)
 
         self.line.set_linestyle("None")
         self.linePercents.set_linestyle("None")
         self.lineDegrees.set_linestyle("None")
+        self.trickLine.set_linestyle("None")
         
         self.line.set_marker(markerStyles[self.marqueur['style']])
         self.linePercents.set_marker(markerStyles[self.marqueur['style']])
         self.lineDegrees.set_marker(markerStyles[self.marqueur['style']])
+        self.trickLine.set_marker(markerStyles[self.marqueur['style']])
         
         self.line.set_markeredgecolor(colors[self.marqueur['color']])
         self.linePercents.set_markeredgecolor(colors[self.marqueur['color']])
         self.lineDegrees.set_markeredgecolor(colors[self.marqueur['color']])
+        self.trickLine.set_markeredgecolor(colors[self.marqueur['color']])
         
         self.line.set_markerfacecolor(colors[self.marqueur['color']])
         self.linePercents.set_markerfacecolor(colors[self.marqueur['color']])
         self.lineDegrees.set_markerfacecolor(colors[self.marqueur['color']])
+        self.trickLine.set_markerfacecolor(colors[self.marqueur['color']])
         
         self.line.set_markersize(self.marqueur['size'])
         self.linePercents.set_markersize(self.marqueur['size'])
         self.lineDegrees.set_markersize(self.marqueur['size'])
+        self.trickLine.set_markersize(self.marqueur['size'])
         
         self.line.set_alpha(self.opacity)
         self.linePercents.set_alpha(self.opacity)
         self.lineDegrees.set_alpha(self.opacity)
+        self.trickLine.set_alpha(self.opacity)
         
         self.line.set_zorder(self.order)
         self.linePercents.set_zorder(self.order)
         self.lineDegrees.set_zorder(self.order)
+        self.trickLine.set_zorder(self.order)
         
         self.line.set_visible(self.markersVisible and self.active)
         self.linePercents.set_visible(self.markersVisible and self.active)
         self.lineDegrees.set_visible(self.markersVisible and self.active)
+        self.trickLine.set_visible(self.markersVisible and self.active)
     
     def __del__(self):
         sProfile.counter -= 1
@@ -121,5 +136,6 @@ class sProfile:
         attributes["line"] = Line2D([], [])
         attributes["linePercents"] = Line2D([], [])
         attributes["lineDegrees"] = Line2D([], [])
+        attributes["trickLine"] = Line2D([], [])
 
         return attributes
