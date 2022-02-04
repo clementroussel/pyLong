28aab00e31fad09945dfc1603426cda47a0baf76
Clément Roussel <clement.roussel@onf.fr>
2022-02-04 14:13:15 +0100

    Update preview.py

--- src/main/python/pyLong/preview.py
+++ src/main/python/pyLong/preview.py
@@ -154,7 +154,7 @@ class Preview:
 
             elif descending_inverted:
                 self.x = np.flip(self.x)
-                self.znp.flip(self.z)
+                self.z = np.flip(self.z)
 
             elif ascending:
                 self.x = self.x[0] + self.x[-1] - np.flip(self.x)
