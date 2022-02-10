6fa14288303c6a5ce80c2fab3c5a9c5e211ef207
Clément Roussel <clement.roussel@onf.fr>
2022-02-10 14:51:42 +0100

    Create setting.py

---
+++ src/main/python/pyLong/setting.py
@@ -0,0 +1,21 @@
+class Setting():
+
+    def __init__(self):
+
+        self.slopeSymbol = "%"
+
+        self.profileDirection = "ascending"
+        
+        self.exportFileFormat = "png"
+        
+        self.figureWidth = 29.7
+        
+        self.figureHeight = 21.0
+        
+        self.figureDpi = 200
+
+        self.reminderLineProperties = {'style': 'dashed',
+                                       'color': 'Black',
+                                       'thickness': 0.8,
+                                       'opacity': 1,
+                                       'order': 1}
