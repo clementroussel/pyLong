939e308a37f719b9f91a5ba03d6c0560a3a35526
Clément Roussel <clement.roussel@onf.fr>
2022-02-12 12:07:43 +0100

    updates

--- src/main/python/interface/dialogAddProfile.py
+++ src/main/python/interface/dialogAddProfile.py
@@ -72,7 +72,7 @@ class DialogAddProfile(QDialog):
         
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Close)
         buttonBox.button(QDialogButtonBox.Close).setText("Close")
-        # buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.importer)
+        buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.importProfile)
         buttonBox.rejected.connect(self.reject)
 
         layout.addWidget(buttonBox)
@@ -80,132 +80,127 @@ class DialogAddProfile(QDialog):
         
         self.setLayout(mainLayout)
 
-    # def updateAxes(self):
-    #     i = self.pyLong.listeLayouts.currentIndex()
-    #     self.layout = self.pyLong.projet.layouts[i]
+    def updateAxis(self):
+        i = self.pyLong.layoutsList.currentIndex()
+        self.layout = self.pyLong.project.layouts[i]
 
-    #     self.pyLong.canvas.ax_z.set_xlim((self.layout.abscisses['min'] - self.layout.abscisses['delta gauche'],
-    #                                       self.layout.abscisses['max'] + self.layout.abscisses['delta droite']))
+        self.pyLong.canvas.ax_z.set_xlim((self.layout.xAxisProperties['min'] - self.layout.xAxisProperties['left shift'],
+                                          self.layout.xAxisProperties['max'] + self.layout.xAxisProperties['right shift']))
 
-    #     self.pyLong.canvas.ax_z.set_xticks(np.linspace(self.layout.abscisses['min'],
-    #                                                    self.layout.abscisses['max'],
-    #                                                    self.layout.abscisses['intervalles'] + 1))
+        self.pyLong.canvas.ax_z.set_xticks(np.linspace(self.layout.xAxisProperties['min'],
+                                                       self.layout.xAxisProperties['max'],
+                                                       self.layout.xAxisProperties['intervals'] + 1))
 
-    #     for ax in self.pyLong.canvas.subplots:
-    #         ax.set_xlim((self.layout.abscisses['min'] - self.layout.abscisses['delta gauche'],
-    #                      self.layout.abscisses['max'] + self.layout.abscisses['delta droite']))
+        for ax in self.pyLong.canvas.subplots:
+            ax.set_xlim((self.layout.xAxisProperties['min'] - self.layout.xAxisProperties['left shift'],
+                         self.layout.xAxisProperties['max'] + self.layout.xAxisProperties['right shift']))
 
-    #         ax.set_xticks(np.linspace(self.layout.abscisses['min'],
-    #                                   self.layout.abscisses['max'],
-    #                                   self.layout.abscisses['intervalles'] + 1))
+            ax.set_xticks(np.linspace(self.layout.xAxisProperties['min'],
+                                      self.layout.xAxisProperties['max'],
+                                      self.layout.xAxisProperties['intervals'] + 1))
 
-    #     self.pyLong.canvas.ax_z.set_ylim((self.layout.altitudes['min'] - self.layout.altitudes['delta bas'],
-    #                                       self.layout.altitudes['max'] + self.layout.altitudes['delta haut']))
+        self.pyLong.canvas.ax_z.set_ylim((self.layout.zAxisProperties['min'] - self.layout.zAxisProperties['lower shift'],
+                                          self.layout.zAxisProperties['max'] + self.layout.zAxisProperties['upper shift']))
 
-    #     self.pyLong.canvas.ax_z.set_yticks(np.linspace(self.layout.altitudes['min'],
-    #                                                    self.layout.altitudes['max'],
-    #                                                    self.layout.altitudes['intervalles'] + 1))
+        self.pyLong.canvas.ax_z.set_yticks(np.linspace(self.layout.zAxisProperties['min'],
+                                                       self.layout.zAxisProperties['max'],
+                                                       self.layout.zAxisProperties['intervals'] + 1))
 
-    #     self.pyLong.canvas.draw()
+        self.pyLong.canvas.draw()
         
     def browse(self):
         path = QFileDialog.getOpenFileName(caption="Add a new profile",
                                            filter="text file (*.txt)")[0]
         self.path.setText(path)
 
-    def importer(self):
-        if self.chemin.text() == "":
-            alerte = QMessageBox(self)
-            alerte.setText("Renseignez un fichier.")
-            alerte.exec_()
-            return 0
+    def importProfile(self):
+        try:
+            profile = pd.read_csv(self.path.text(),
+                                  delimiter=delimiters[self.delimiter.currentText()],
+                                  decimal=separators[self.separator.currentText()],
+                                  skiprows=0,
+                                  encoding='utf-8').values
 
-        else:
-            try:
-                profil = pd.read_csv(self.chemin.text(),
-                                     delimiter=delimiteurs[self.delimiteur.currentText()],
-                                     decimal=separateurs[self.separateur.currentText()],
-                                     skiprows=0,
-                                     encoding='utf-8').values
+            xz = np.array(profile[:,:2].astype('float'))
 
-                xz = np.array(profil[:,:2].astype('float'))
+            if np.shape(xz[:,0])[0] < 2:
+                alerte = QMessageBox(self)
+                alerte.setText("Import failed : Profile must contain at least 2 points.")
+                alerte.exec_()
+                return 0
 
-                if np.shape(xz[:,0])[0] < 2:
-                    alerte = QMessageBox(self)
-                    alerte.setText("Le profil doit contenir au moins 2 points.")
-                    alerte.exec_()
-                    return 0
+            else:
+                zprofile = zProfile()
 
+                zprofile.title = self.title.text()
+                zprofile.x = xz[:,0]
+                zprofile.z = xz[:,1]
+
+                zprofile.sort(mode=self.pyLong.project.settings.profileDirection)
+                zprofile.update()
+
+                if zprofile.z[0] == xz[0, 1]:
+                    sorted = False
                 else:
-                    zprofil = zProfil()
+                    sorted = True
 
-                    zprofil.intitule = self.intitule.text()
-                    zprofil.abscisses = xz[:,0]
-                    zprofil.altitudes = xz[:,1]
+                sprofile = sProfile()
+                sprofile.updateData(zprofile.x, zprofile.z)
+                sprofile.update()
 
-                    zprofil.trier(mode=self.pyLong.projet.preferences['sens'])
-                    zprofil.update()
+                self.pyLong.project.profiles.append((zprofile, sprofile))
+                self.pyLong.profilesList.update()
+                self.pyLong.canvas.ax_z.add_line(zprofile.line)
+                self.pyLong.canvas.updateLegends()
 
-                    if zprofil.altitudes[0] == xz[0, 1]:
-                        tri = False
-                    else:
-                        tri = True
+                if len(self.pyLong.project.profiles) == 1:
+                    i = self.pyLong.layoutsList.currentIndex()
+                    self.layout = self.pyLong.project.layouts[i]
 
-                    pprofil = pProfil()
-                    pprofil.updateData(zprofil.abscisses, zprofil.altitudes)
-                    pprofil.update()
+                    self.layout.xAxisProperties['min'] = np.min(zprofile.x)
+                    self.layout.xAxisProperties['max'] = np.max(zprofile.x)
 
-                    self.pyLong.projet.profils.append((zprofil, pprofil))
-                    self.pyLong.listeProfils.update()
-                    self.pyLong.canvas.ax_z.add_line(zprofil.line)
-                    self.pyLong.canvas.updateLegendes()
+                    self.layout.zAxisProperties['min'] = np.min(zprofile.z)
+                    self.layout.zAxisProperties['max'] = np.max(zprofile.z)
 
-                    if len(self.pyLong.projet.profils) == 1:
-                        i = self.pyLong.listeLayouts.currentIndex()
-                        self.layout = self.pyLong.projet.layouts[i]
+                    self.updateAxis()
 
-                        self.layout.abscisses['min'] = np.min(zprofil.abscisses)
-                        self.layout.abscisses['max'] = np.max(zprofil.abscisses)
+            if self.importAnnotations.isChecked():
+                try:
+                    annotations = list(profile[:, 2])
+                    for i, label in enumerate(annotations):
+                        if label is not np.nan and str(label) != 'nan':
+                            annotation = VerticalAnnotation()
+                            annotation.label = str(label)
+                            annotation.title = str(label)
 
-                        self.layout.altitudes['min'] = np.min(zprofil.altitudes)
-                        self.layout.altitudes['max'] = np.max(zprofil.altitudes)
+                            if not sorted:
+                                annotation.position['x coordinate'] = xz[i, 0]
+                            else:
+                                n = len(annotations)
+                                annotation.position['x coordinate'] = zprofile.x[n-1-i]
 
-                        self.updateAxes()
+                            annotation.position['z coordinate'] = xz[i, 1]
+                            annotation.update()
 
-                if self.annotations.isChecked():
-                    try:
-                        annotations = list(profil[:, 2])
-                        for i, libelle in enumerate(annotations):
-                            if libelle is not np.nan and str(libelle) != 'nan':
-                                ap = AnnotationPonctuelle()
-                                ap.libelle = str(libelle)
-                                ap.intitule = str(libelle)
+                            j = self.pyLong.annotationsList.groups.currentIndex()
+                            self.pyLong.project.groups[j].annotations.append(annotation)
 
-                                if not tri:
-                                    ap.abscisse = xz[i, 0]
-                                else:
-                                    n = len(annotations)
-                                    ap.abscisse = zprofil.abscisses[n-1-i]
+                            self.pyLong.canvas.ax_z.add_artist(annotation.annotation)
 
-                                ap.altitude = xz[i, 1]
-                                ap.update()
-
-                                j = self.pyLong.listeAnnotations.groupes.currentIndex()
-                                self.pyLong.projet.groupes[j].annotations.append(ap)
-
-                                self.pyLong.canvas.ax_z.add_artist(ap.annotation)
-
-                        self.pyLong.listeAnnotations.updateListe()
-                        self.pyLong.canvas.draw()
-                        self.accept()
-
-                    except:
-                        self.accept()
-
-                else:
+                    # self.pyLong.annotationsList.updateList()
+                    self.pyLong.canvas.draw()
                     self.accept()
 
-            except:
-                alerte = QMessageBox(self)
-                alerte.setText("Fichier illisible.")
-                alerte.exec_()
+                except:
+                    alerte = QMessageBox(self)
+                    alerte.setText("Annotations import failed.")
+                    alerte.exec_()
+
+            else:
+                self.accept()
+
+        except:
+            alerte = QMessageBox(self)
+            alerte.setText("Import failed.")
+            alerte.exec_()
